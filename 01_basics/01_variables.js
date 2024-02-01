e1af97dc45cec734c2e499fdbfc8699e59f425f9
shubham-rk <158461105+shubham-rk@users.noreply.github.com>
2024-02-01 11:10:12 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "Shubham@gmail.com"
+var accountPassword = "12345"
+accountCity  = "jairpur"
+
+
+// accountId = 2
+accountEmail = "fajshfa@gmail.com"
+accountPassword = "743890570293"
+accountCity = "Bengaluru"
+
+console.log(accountCity);
+console.log(accountPassword);
+
+console.table([accountEmail, accountCity, accountPassword]);
+
+/*
+do not use var 
+because of block scope and function scope
+*/
+
+console.log("Hello");
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using node js not browser so its ot running
+
+console.log(3+3) //code redability should have
+let name = "hitesh"
+let age = 18
+let isLoggedIN = false
+
+// number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> 
+// symbol 
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null)
